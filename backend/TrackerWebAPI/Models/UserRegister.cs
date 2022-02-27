fe380dbb85a80e1d4fbb87e98cdb85d46ba69a87
Roope Uitto <roouit@gmail.com>
2022-02-27 11:35:16 +0200

    Add restrictions to UserRegister model

--- backend/TrackerWebAPI/Models/UserRegister.cs
+++ backend/TrackerWebAPI/Models/UserRegister.cs
@@ -4,17 +4,25 @@ namespace TrackerWebAPI.Models
 {
     public class UserRegister
     {
-        [Required]
+        [Required(ErrorMessage = "Username can't be empty")]
+        [MinLength(2, ErrorMessage = "Username must have at least 2 characters")]
+        [MaxLength(128, ErrorMessage = "Maximum length of a username is 128 characters")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password can't be empty")]
+        [MinLength(8, ErrorMessage = "Password must have at least 8 characters")]
+        [MaxLength(55, ErrorMessage = "Maximum length of a password is 55 characters")]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Email can't be empty")]
+        [EmailAddress(ErrorMessage = "Email address is not valid")]
+        [MaxLength(256, ErrorMessage = "Maximum length of an email address is 256 characters")]
         public string Email { get; set; }
 
+        [MaxLength(128, ErrorMessage = "Maximum length of a first name is 128 characters")]
         public string FirstName { get; set; }
 
+        [MaxLength(128, ErrorMessage = "Maximum length of a last name is 128 characters")]
         public string LastName { get; set; }
     }
 }
