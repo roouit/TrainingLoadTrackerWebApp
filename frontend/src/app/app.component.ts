c8a526f0944364ca615d34e1095cdbb95771e697
Roope Uitto <roouit@gmail.com>
2022-04-23 08:47:20 +0300

    Keep sidenav visible on desktop view
    Opening and closing sidenav is disabled when on desktop view.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'TLOAD Tracker';
+  title: string = 'TLOAD Tracker';
+  mobileView!: boolean;
+  public innerWidth: any;
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    this.mobileView = this.innerWidth > 800 ? false : true
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+    this.mobileView = this.innerWidth > 800 ? false : true;
+  }
 }
--- frontend/src/app/core/components/side-navigation/side-navigation.component.ts
+++ frontend/src/app/core/components/side-navigation/side-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import { UserApiService } from '../../services/user-api.service';
@@ -10,14 +10,31 @@ import { UserApiService } from '../../services/user-api.service';
 })
 export class SideNavigationComponent implements OnInit {
   @Input() sidenav!: MatSidenav;
+  mobileView!: boolean;
+  public innerWidth: any;
 
   constructor(public userApiService: UserApiService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    this.mobileView = this.innerWidth > 800 ? false : true;
+  }
 
   logout() {
     this.userApiService.logout();
     this.sidenav.toggle();
     this.router.navigate(['/auth/login']);
+  }
+
+  changePageAction() {
+    if (this.mobileView) {
+      this.sidenav.toggle()
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+    this.mobileView = this.innerWidth > 800 ? false : true;
   }
 }
